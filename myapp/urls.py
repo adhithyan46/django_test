23ce7a82766f5e4169f6631a78230fb1121df4b1
Adhithyan <ksadhithyan100@gmail.com>
2024-08-03 15:08:40 -0700

    first commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('',views.mypage,name='mypage'),
+    path('',views.login_page,name='login_page'),
+    path('register_page/',views.register_page,name="register_page"),
+    path('search_page/',views.search_page,name="search_page"),
+
+
 ]
